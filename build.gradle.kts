e46ad567c4ac9e8171aa1f08a508f7e03cc13ebd
Jesus Zazueta <jjzazuet@vacco.io>
2021-03-12 01:46:47 -0500

    - POM license fix.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins { id("io.vacco.oss") version "1.0.0" }
 
 group = "io.vacco.jlame"
-version = "3.100.1"
+version = "3.100.2"
 
 configure<io.vacco.oss.CbPluginProfileExtension> {
   addJ8Spec()
